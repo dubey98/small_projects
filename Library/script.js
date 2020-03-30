5220fc9de1d1e4d4dde06218c68f47bea9019427
shiv dubey <dubeyshivprasad98@gmail.com>
2020-03-30 21:28:30 +0530

    Library: implemented basic behaviour
    deleted form.html and implemented the logic in index.html

--- Library/script.js
+++ Library/script.js
@@ -1,43 +1,98 @@
-function Book(title,author,pages,read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    // this.info = function(){
-    //     if(read === "read"){
-    //         return title + " by " + author + "," + pages + 
-    //                 " pages," + "read.";
-    //     }
-    //     else{
-    //         return title + " by " + author + "," + pages + 
-    //                 " pages," + "not read yet.";
-    //     }
-    // }
-}
+// Declared an array where the Book Objects are to be stored
 let myLibrary  = [];
-function addBookToLibrary(){
-    let hobbit = new Book("the Hobbit","Dont know",456,"not read yet");
-    myLibrary.push(hobbit);
-    let harry = new Book("Harry potter","JK Rowling",789,"read");
-    myLibrary.push(harry);
-};
-addBookToLibrary();
-
+class Book {
+    //Constructor for the Book Class
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        //taking input from a radio button where value is "yes" or "no"
+        if(read === "yes")this.read = true;
+        else this.read = false;
+    }
+    //method to toggle the readstatus of the book
+    readStatus(){
+        if (this.read === false){
+            this.read = true;
+        }
+        else{
+            this.read = false;
+        }
+        render();
+    }
+    //function to get the readstatus of the Book
+    getStatus() {
+        return this.read;
+    }
+}
+//display function 
 let render = function() {
-    let table = document.querySelector('table');
+    const table = document.querySelector('table');
+    //deleting the previously declared table rows or else they complile on top of each other
+    //and new rows are added contantly
+    const pre = document.querySelectorAll('.tableRow')
+    for(let i=0;i<pre.length;i++){
+        pre[i].remove();
+    }
+
+    //Looping through all the Books in myLibrary array
     for(let i=0;i<myLibrary.length;i++){
+        //creating a new row element
         let new_row = document.createElement('tr');
-        let j=0;
         for(let prop in myLibrary[i]){
-            let new_col = document.createElement('td');
-            new_col.innerHTML = myLibrary[i][prop];
+            //creating a new column element
+            const new_col = document.createElement('td');
+            new_col.innerText = myLibrary[i][prop];
             new_row.appendChild(new_col);
         }
+        //declaring a button for removing the element from array
+        const button = document.createElement('button');
+        button.innerText = "Remove";
+        //setting the id equal to array index of the element
+        const newLocal = "`${i}`";
+        button.setAttribute("id",newLocal);
+        button.classList.add('remove');
+        new_row.appendChild(button);
+        button.addEventListener('click',(e)=>{
+            let temp = e.toElement.id;
+            let i = parseInt(temp);
+            myLibrary.splice(i,1);
+            render();
+        });
+
+        //button for toggling the read status of the book
+        const readStatus = document.createElement('button');
+        readStatus.innerText = "Read";
+        //setting the id to be the array index for the button
+        const newLocal_1 = String(i);
+        readStatus.setAttribute("id",newLocal_1);
+        new_row.appendChild(readStatus);
+        readStatus.addEventListener('click',(e)=>{
+            let i = parseInt(e.toElement.id);
+            if(myLibrary[i].getStatus()){
+                myLibrary[i].readStatus();
+            }else{
+                myLibrary[i].readStatus();
+            }
+        });
+        
+        new_row.classList.add('tableRow');
         table.appendChild(new_row);
     }
 }
-render();
-
-document.addEventListener('form.submit()',(e)=>{
-    console.log(e);
-})
+//function for adding books to library from the form 
+function addBookToLibrary(){
+    document.forms.newBook.addEventListener('submit',function handleform(e){
+        let form = document.forms.newBook;
+        let newBook = new Book(form.elements.name.value,
+                form.elements.author.value,
+                form.elements.pages.value,
+                form.elements.status.value);
+        myLibrary.push(newBook);
+        //prevents the form from refreshing which causes the loass of data stored
+        e.preventDefault();
+        render();
+    });
+    render();
+};
+addBookToLibrary();
